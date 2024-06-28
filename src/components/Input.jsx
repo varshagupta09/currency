8a11917e2b5e73bf0ae33dae34e094d0eb6a852b
Varsha Gupta <vrsa8896@gmail.com>
2024-06-28 18:47:53 +0530

    ...

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useId} from "react";
 
 const Input = ({
   label,
@@ -11,11 +11,14 @@ const Input = ({
   currencyDisable = false,
   className = "",
 }) => {
+
+    const amountInputId = useId();
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex `}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">{label}</label>
         <input
+        id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
